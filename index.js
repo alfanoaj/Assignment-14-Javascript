e72cf1792d34a26f85de2b4d2c11f165a31f5219
Anthony Alfano <alfanoaj@gmail.com>
2018-06-23 23:28:19 -0500

    Working upload

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// declare variables to be used later in the document
 var tbody = document.querySelector("tbody");
 var date = document.querySelector("#date");
 var city = document.querySelector("#city");
 var searchBtn = document.querySelector("#search");
+var clearBtn = document.querySelector("#clear");
 var selState = document.querySelector("#selState");
 var selCountry = document.querySelector("#selCountry");
 var selShape = document.querySelector("#selShape");
@@ -9,48 +11,47 @@ var selShape = document.querySelector("#selShape");
 // Set filteredAddresses to addressData initially
 var filtered = dataSet;
 
+// create a unique array of countries and set that list to the countries dropdown
 var countries = filtered.map(x => x.country);
 var uniqueCountries = [...new Set(countries)];
-
 for (var c = 0; c < uniqueCountries.length; c++) {
   var opt = document.createElement("option");
   opt.innerText = uniqueCountries[c];
   selCountry.appendChild(opt);
 }
 
+// create a unique array of shapes and set that list to the shapes dropdown
 var shapes = filtered.map(x => x.shape);
 var uniqueShapes = [...new Set(shapes)];
-
 for (var c = 0; c < uniqueShapes.length; c++) {
   var opt = document.createElement("option");
   opt.innerText = uniqueShapes[c];
   selShape.appendChild(opt);
 }
 
+// create a unique array of states and set that list to the states dropdown
 var states = filtered.map(x => x.state);
 var uniqueStates = [...new Set(states)];
-
 for (var c = 0; c < uniqueStates.length; c++) {
   var opt = document.createElement("option");
   opt.innerText = uniqueStates[c];
   selState.appendChild(opt);
 }
 
-
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
 searchBtn.addEventListener("click", handleSearchButtonClick);
 
-// renderTable renders the filteredAddresses to the tbody
+// renderTable renders the filtered to the tbody
 function renderTable() {
   tbody.innerHTML = "";
   for (var i = 0; i < filtered.length; i++) {
-    // Get get the current address object and its fields
+    // Get get the current object and its fields
     var date = filtered[i];
     var fields = Object.keys(date);
     // Create a new row in the tbody, set the index to be i + startingIndex
     var row = tbody.insertRow(i);
     for (var j = 0; j < fields.length; j++) {
-      // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
+      // For every field in the  object, create a new cell at set its inner text to be the current value at the current field
       var field = fields[j];
       var cell = row.insertCell(j);
       cell.innerText = date[field];
@@ -58,9 +59,11 @@ function renderTable() {
   }
 }
 
+// define a function to handle our search filters
 function handleSearchButtonClick() {
+  // declare an empty object to be appended later
   var filter = {};
-  // Format the user's search by removing leading and trailing whitespace, lowercase the string
+  // format the date input so the user can use MM/DD/YYYY formatting
   if (date.value != "") {
     var day = date.value.slice(0, 2);
     var month = date.value.slice(3, 5);
@@ -80,21 +83,26 @@ function handleSearchButtonClick() {
 
     filter["datetime"] = filterDate;
   }  
+
+  // Appends filter with the user input into the city form. If the input is blank, do nothing
   if (city.value != "") {
     var filterCity = city.value.trim().toLowerCase();
     filter["city"] = filterCity;
   }
 
+  // gets the current list item from the country dropdown option and appends filter. Does nothing if set to "All"
   var country = selCountry.options[selCountry.selectedIndex].text;
   if (country != "All") {
     filter["country"] = country;
   }
 
+  // gets the current list item from the state dropdown option and appends filter. Does nothing if set to "All"
   var state = selState.options[selState.selectedIndex].text;
   if (state != "All") {
     filter["state"] = state;
   }
 
+  // gets the current list item from the shape dropdown option and appends filter. Does nothing if set to "All"
   var shape = selShape.options[selShape.selectedIndex].text;
   if (shape != "All") {
     filter["shape"] = shape;
@@ -102,6 +110,7 @@ function handleSearchButtonClick() {
 
   console.log(filter);
 
+  // iterate through the keys in the filter object and filter our table accordingly
   filtered = filtered.filter(function(item) {
     for (var key in filter) {
       if (item[key] === undefined || item[key] != filter[key])
@@ -109,16 +118,6 @@ function handleSearchButtonClick() {
     }
     return true;
   });
-  
-  console.log(filtered);
-
-    // // Set filteredAddresses to an array of all addresses whose "state" matches the filter
-    // filtered = dataSet.filter(function(date) {
-    //   var newDate = date.datetime;
-
-    //   // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
-    //   return newDate == filterDate;
-    // });
     renderTable();
 }
 
